450cbe4e518566243db12d35b724bdc2ea1cad75
traceback <mehran1990ss@yahoo.com>
2019-10-11 20:41:59 +0330

    Packages

--- packages/Mehran/UploadMe/src/Config/menu.php
+++ packages/Mehran/UploadMe/src/Config/menu.php
@@ -4,7 +4,7 @@ return [
         'key' => 'UploadMe',          // uniquely defined key for menu-icon
         'name' => 'Upload',        //  name of menu-icon
         'route' => 'UploadMe.index',  // the route for your menu-icon
-        'sort' => 2,                    // Sort number on which your menu-icon should display
+        'sort' => 6,                    // Sort number on which your menu-icon should display
         'icon-class' => 'folder-icon',   //class of menu-icon
     ]
 ];
